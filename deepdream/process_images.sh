eeab0fce3cae235bc17b409e4412e203f3233f92
Dom Dümmler <info@rainbat.ch>
2017-04-20 00:56:53 +0200

    modified for batch

--- deepdream/process_images.sh
+++ deepdream/process_images.sh
@@ -5,6 +5,14 @@
 
 while [ true ];
 do
-    ./process_images_once.sh
+    ./process_images_once.sh "1.5" "inception_4c/output"    
+    ./process_images_once.sh "3.0" "inception_4c/output"    
+    ./process_images_once.sh "4.5" "inception_4c/output"    
+    ./process_images_once.sh "1.5" "inception_3b/5x5_reduce"
+    ./process_images_once.sh "3.0" "inception_3b/5x5_reduce"
+    ./process_images_once.sh "4.5" "inception_3b/5x5_reduce"
+    ./process_images_once.sh "1.5" "conv2/3x3"
+    ./process_images_once.sh "3.0" "conv2/3x3"
+    ./process_images_once.sh "4.5" "conv2/3x3"
     sleep 1
 done
--- deepdream/process_images_once.sh
+++ deepdream/process_images_once.sh
@@ -3,23 +3,33 @@
 # We will be running this script inside the visionai/clouddream Docker image
 # Copyright vision.ai, 2015
 
+file1=$(echo "$1")
+file2=$(echo "$2" | sed -e "s/\//-/g")
+
 cd /opt/deepdream/inputs
+
 find . -type f -not -path '*/\.*' -print0 | while read -d $'\0' f;
 do
     cd /opt/deepdream
-    if [ -e outputs/${f} ];
+    
+    insert="_${file2}_${file1}"
+    filename=$(echo "${f}" | sed "s/.jpg/$insert.jpg/g")
+    
+    echo "processing with $filename"
+
+    if [ -e outputs/$filename ];
     then
-	echo "File ${f} already processed"
+		echo "... File $filename already processed"
     else
-	echo "Deepdream" ${f}
-	chmod gou+r inputs/${f}
-	cp inputs/${f} input.jpg
-	python deepdream.py
-	ERROR_CODE=$?
-	echo "Error Code is" ${ERROR_CODE}
-	cp output.jpg outputs/${f}
-	rm output.jpg
-	echo "Just created" outputs/${f}
+		echo "... Deepdream $filename"
+		chmod gou+r inputs/${f}
+		cp inputs/${f} input.jpg
+		python deepdream.py $1 $2
+		ERROR_CODE=$?
+		echo "......Error Code is" ${ERROR_CODE}
+		cp output.jpg outputs/$filename
+		rm output.jpg
+		echo "...Just created" outputs/$filename
     fi
 done
 
