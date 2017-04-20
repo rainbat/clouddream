fc87a9dfbbeca992b2e6bc456f8bd9efcd47b0b0
Alex Furer <alex@fullframestudios.ch>
2017-04-20 03:15:54 +0200

    changed to source all.sh

--- deepdream/process_images.sh
+++ deepdream/process_images.sh
@@ -5,14 +5,22 @@
 
 while [ true ];
 do
-    ./process_images_once.sh "1.0"  "inception_4c/output"    
-    ./process_images_once.sh "5.0"  "inception_4c/output"    
-    ./process_images_once.sh "10.0" "inception_4c/output"    
-    ./process_images_once.sh "1.0"  "inception_3b/5x5_reduce"
-    ./process_images_once.sh "5.0"  "inception_3b/5x5_reduce"
-    ./process_images_once.sh "10.0" "inception_3b/5x5_reduce"
-    ./process_images_once.sh "1.0"  "conv2/3x3"
-    ./process_images_once.sh "5.0"  "conv2/3x3"
-    ./process_images_once.sh "10.0" "conv2/3x3"
+    
+
+    #./process_images_once.sh "1.0"  "inception_4c/output"    
+    #./process_images_once.sh "5.0"  "inception_4c/output"    
+    #./process_images_once.sh "10.0" "inception_4c/output"    
+    #./process_images_once.sh "1.0"  "inception_3b/5x5_reduce"
+    #./process_images_once.sh "5.0"  "inception_3b/5x5_reduce"
+    #./process_images_once.sh "10.0" "inception_3b/5x5_reduce"
+    #./process_images_once.sh "1.0"  "conv2/3x3"
+    #./process_images_once.sh "5.0"  "conv2/3x3"
+    #./process_images_once.sh "10.0" "conv2/3x3"
+
+    source all.sh
+
+
+
+
     sleep 1
 done
